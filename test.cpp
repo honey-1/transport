c4dc183a988ea9946896f22b92d4c9e067a24d7f
yunzimo <45143713+yunzimo@users.noreply.github.com>
2020-04-23 09:40:13 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+int a[40];
+int V,n;
+int f[20010];
+
+int main()
+{
+    cin>>V>>n;
+    for(int i=1;i<=n;i++)
+        cin>>a[i];
+    for(int i=1;i<=n;i++)
+        for(int j=V;j>=a[i];j--)
+            f[j]=max(f[j],f[j-a[i]]+a[i]);
+    cout<<V<<"/"<<f[V];
+    return 0;
+}
